9a47b3f9d74f1f9341aa032aa2543bef7c2244e0
BenjaminSchnorr <benschnorr1@gmail.com>
2024-04-29 10:16:24 -0600

    Key and Secret Key

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@
 
 // Clyncke Key: PKBIBI0HHEZKJ9GQXPKI
 // Clyncke SecretKey: SqKPm3Q1SbZqjGKuN5FvGR1VrVxtBpAd8EtNGcu7
+// Ben: PKHNM3UBEU8L3QLLS2NL
+// SecretKey: rNkaJFDjRaCIjtIkJoSILKyHhk961ofbHIHWOpdF
 
 
 public class Main {
